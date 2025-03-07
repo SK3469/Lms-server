0a285b7b26089e7fc74319c72fcada6695f7eed7
sunil kumar <sunilk3469@gmail.com>
2025-03-07 12:45:34 +0530

    update

--- index.js
+++ index.js
@@ -17,11 +17,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ["https://lms-ui-bcym.onrender.com","http://localhost:5173"],// ✅ Allow your frontend domain
-     credentials: true,
+    origin: ["https://lms-ui-bcym.onrender.com", "http://localhost:5173"],// ✅ Allow your frontend domain
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["set-cookie"]
 }));
 
 
